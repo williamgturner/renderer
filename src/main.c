e0ed649691ed6de3aaa3896037668d89751bd455
William Turner <williamguyturner@proton.me>
2025-10-05 23:06:39 +1300

    remove boilerplate text

--- src/main.c
+++ src/main.c
@@ -12,7 +12,6 @@
 #include "wallHitDto.h"
 #include <math.h>
 
-/* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static RenderContext *ctx = NULL;
@@ -22,7 +21,6 @@ static GameSpace *game = NULL;
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
-/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   int i;
 
@@ -58,7 +56,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   game->worldScale = 64;
   game->map = generate_map(10, 10);
 
-  /* Open the font */
   ctx->font = TTF_OpenFont("../assets/fonts/HomeVideo-BLG6G.ttf", 15);
   if (!ctx->font) {
     SDL_Log("Couldn't open font: %s\n", SDL_GetError());
@@ -70,24 +67,22 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   return SDL_APP_CONTINUE;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   if (event->type == SDL_EVENT_QUIT) {
-    return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+    return SDL_APP_SUCCESS;
   }
-  return SDL_APP_CONTINUE; /* carry on with the program! */
+  return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
   const Uint64 now = SDL_GetTicks();
   const float elapsed =
-      ((float)(now - last_time)) / 1000.0f; /* seconds since last iteration */
+      ((float)(now - last_time)) / 1000.0f;
   int i;
   last_time = now;
 
   SDL_SetRenderDrawColor(renderer, 0, 0, 0,
-                         SDL_ALPHA_OPAQUE); /* black, full alpha */
+                         SDL_ALPHA_OPAQUE);
   SDL_RenderClear(renderer);
   handleInput(game, elapsed);
 
@@ -105,7 +100,6 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   if (ctx) {
     if (ctx->font) {
